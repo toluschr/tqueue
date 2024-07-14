b8d0b66748889c0f3de8c874e14654afbe544876
toluschr <toluschr@protonmail.com>
2024-07-14 16:35:17 +0200

    Add more tests

--- test.c
+++ test.c
@@ -7,6 +7,152 @@
 
 #include <errno.h>
 
+static void test_queue_insert_and_remove_with_end(void)
+{
+    struct tqueue q;
+    assert(tqueue_init(&q) == 0);
+
+    struct tqueue_node *i = malloc(tqueue_sizeof(int));
+    assert(i != NULL);
+
+    *(int *)i->data = 0;
+
+    while (tqueue_put_node(&q, i) < 0) {
+        assert(errno == EINTR);
+    }
+
+    while (tqueue_put_node(&q, NULL) < 0) {
+        assert(errno == EINTR);
+    }
+
+    while (tqueue_get_node(&q, &i) < 0) {
+        assert(errno == EINTR);
+    }
+
+    assert(i != NULL);
+    free(i);
+
+    while (tqueue_get_node(&q, &i) < 0) {
+        assert(errno == EINTR);
+    }
+
+    assert(i == NULL);
+
+    assert(tqueue_fini(&q) == 0);
+}
+
+static void test_queue_length_zero_with_end(void)
+{
+    struct tqueue q;
+    assert(tqueue_init(&q) == 0);
+
+    while (tqueue_put_node(&q, NULL) < 0) {
+        assert(errno == EINTR);
+    }
+
+    assert(tqueue_length(&q) == 0);
+
+    assert(tqueue_fini(&q) == 0);
+}
+
+static void test_queue_length_zero_without_end(void)
+{
+    struct tqueue q;
+    assert(tqueue_init(&q) == 0);
+
+    assert(tqueue_length(&q) == 0);
+
+    assert(tqueue_fini(&q) == 0);
+}
+
+static void test_queue_length_with_end(void)
+{
+    struct tqueue q;
+    struct tqueue_node *i;
+
+    assert(tqueue_init(&q) == 0);
+
+    i = malloc(tqueue_sizeof(int));
+    assert(i != NULL);
+    *(int *)i->data = 0;
+
+    while (tqueue_put_node(&q, i) < 0) {
+        assert(errno == EINTR);
+    }
+
+    i = malloc(tqueue_sizeof(int));
+    assert(i != NULL);
+    *(int *)i->data = 1;
+
+    while (tqueue_put_node(&q, i) < 0) {
+        assert(errno == EINTR);
+    }
+
+    while (tqueue_put_node(&q, NULL) < 0) {
+        assert(errno == EINTR);
+    }
+
+    assert(tqueue_length(&q) == 2);
+
+    while (tqueue_tryget_node(&q, &i) < 0) {
+        assert(errno == EINTR);
+    }
+
+    assert(i != NULL);
+    free(i);
+
+    while (tqueue_tryget_node(&q, &i) < 0) {
+        assert(errno == EINTR);
+    }
+
+    assert(i != NULL);
+    free(i);
+
+    while (tqueue_tryget_node(&q, &i) < 0) {
+        assert(errno == EINTR);
+    }
+
+    assert(i == NULL);
+
+    assert(tqueue_length(&q) == 0);
+
+    assert(tqueue_fini(&q) == 0);
+}
+
+static void test_queue_length_without_end(void)
+{
+    struct tqueue q;
+    assert(tqueue_init(&q) == 0);
+
+    struct tqueue_node *i = malloc(tqueue_sizeof(int));
+    assert(i != NULL);
+    *(int *)i->data = 1;
+
+    while (tqueue_put_node(&q, i) < 0) {
+        assert(errno == EINTR);
+    }
+
+    while (tqueue_put_node(&q, i) < 0) {
+        assert(errno == EINTR);
+    }
+
+    assert(tqueue_length(&q) == 2);
+
+    while (tqueue_tryget_node(&q, &i) < 0) {
+        assert(errno == EINTR);
+    }
+
+    while (tqueue_tryget_node(&q, &i) < 0) {
+        assert(errno == EINTR);
+    }
+
+    free(i);
+
+    assert(tqueue_length(&q) == 0);
+
+    assert(tqueue_fini(&q) == 0);
+}
+
 static void test_tryget_terminated_queue(void)
 {
     struct tqueue q;
@@ -58,8 +204,13 @@ static void test_tryget_empty_queue(void)
     assert(tqueue_fini(&q) == 0);
 }
 
-int main()
+int main(void)
 {
+    test_queue_insert_and_remove_with_end();
+    test_queue_length_zero_with_end();
+    test_queue_length_zero_without_end();
+    test_queue_length_with_end();
+    test_queue_length_without_end();
     test_tryget_terminated_queue();
     test_tryget_empty_queue();
 }
